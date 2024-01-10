bd2eeb9d060368f720af46fd80c6819978f920e5
digitalnand <93953223+digitalnand@users.noreply.github.com>
2024-01-10 19:54:14 -0300

    chore: rename function

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,17 @@ struct Node {
     uint32_t frequency = 0;
 };
 
-auto analyze_frequency(const std::string_view input) -> std::vector<Node> {
+auto extract_frequencies(const std::string_view input) -> std::vector<Node> {
     std::vector<Node> node_list;
-    std::map<char, Node> symbols_lookup{};
+    std::map<char, Node> symbol_lookup{};
 
     for(const char& character : input) {
-        auto& node = symbols_lookup[character];
+        auto& node = symbol_lookup[character];
         node.symbol = character;
         node.frequency++;
     }
 
-    for (const auto& [_, node] : symbols_lookup) {
+    for(const auto& [_, node] : symbol_lookup) {
         node_list.push_back(node);
     }
 
@@ -30,7 +30,7 @@ auto analyze_frequency(const std::string_view input) -> std::vector<Node> {
 auto main() -> int32_t {
     std::string input = "hello world";
 
-    const auto node_list = analyze_frequency(input);
+    const auto node_list = extract_frequencies(input);
 
     for(const auto& node : node_list) {
         std::cout << std::format("symbol: {} | frequency: {}\n", node.symbol, node.frequency);
